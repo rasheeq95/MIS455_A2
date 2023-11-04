ffc22161395417e51afd85ab179e0b327261e9cf
rasheeq95 <116834310+rasheeq95@users.noreply.github.com>
2023-11-04 16:52:46 +0600

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,13 @@
+function process(){
+
+    var person = document.getElementById(" nameBox").value ; 
+    var courses = parseInt(document.getElementById(" idbox").value) ; 
+    
+    
+    
+         document.getElementById("content").innerHTML= `
+        <h4> Sorry ${person}, no scholarship can be obtained this time </h4>`;
+      }
+  }
+       
+
